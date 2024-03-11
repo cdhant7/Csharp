7ed7608a968af7c9b3f84c5c78c7f54b6a0f2187
Sidhant Acharya <acharyasid1@gmail.com>
2024-03-11 23:51:28 +0545

    .

--- Csharp/Program.cs
+++ Csharp/Program.cs
@@ -35,3 +35,31 @@ string name = Console.ReadLine();
 Console.Write("Enter your age ");
 string age = Console.ReadLine();
 Console.WriteLine("Hello " + name + " your age is " + age);
+Console.ReadLine();
+
+//simple calculator
+Console.Write("enter a number ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("enter another number ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(num1 + num2);
+
+//madlib
+string color, pluralNoun, celebrity;
+Console.Write("Enter a color");
+color = Console.ReadLine();
+
+Console.Write("Enter a pluralNoun");
+pluralNoun = Console.ReadLine();
+
+Console.Write("Enter a celebrity");
+celebrity = Console.ReadLine();
+
+Console.WriteLine("Roses are " + color);
+Console.WriteLine(pluralNoun +" are blue ");
+Console.WriteLine("I love " + celebrity);
+Console.ReadLine();
+
+//arrays
+int[] showNumbers = {2,4,6,8,13,56,34,66,88 };
+Console.WriteLine(showNumbers[5]);
